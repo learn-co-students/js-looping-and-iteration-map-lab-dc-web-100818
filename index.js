32799b0a6b169438b8d6d9fe206759e8c6197f91
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-18 12:15:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  let lower_array = array.map(function(name) {
+    return name.toLowerCase();
+  });
+  return lower_array
+}
+
+const drivers = [
+  'Bobby Smith',
+  'Sammy Watkins',
+  'Sally Jenkins',
+  'Annette Sawyer',
+  'Sarah Hucklebee',
+  'bobby ridge'
+];
+
+function nameToAttributes(array) {
+  return array.map(driverName => {
+    let obj = {}
+    let firstName = driverName.split(" ")[0]
+    let lastName = driverName.split(" ")[1]
+    
+    obj["firstName"] = firstName;
+    obj["lastName"] = lastName
+
+    return obj;
+  });
+}
+
+function attributesToPhrase(array) {
+  return array.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
